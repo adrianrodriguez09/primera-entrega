cc776e266e117e28fba9192172df5d7cecbe3c27
adrianrodriguez09 <103323248+adrianrodriguez09@users.noreply.github.com>
2022-06-25 14:51:57 -0300

    arreglando

--- js/storage.js
+++ js/storage.js
@@ -1,12 +1,13 @@
 btnCargar.addEventListener("click", ()=>{
-    const obj = {
-        servicio: inputServicio.value,
-         importe: inputImporte.value
-    }
-    listadoServ.push(obj)
-    console.log("se agergo un servicio")
-    btnCancelar.click()
-    localStorage.setItem("ListadoServ", JSON.stringify(listadoServ))
+const obj = {
+    servicio: inputServicio.value,
+    importe: inputImporte.value
+}
+listadoServ.push(obj)
+console.log("se agrego un servicio")
+btnCancelar.click()
+inputServicio.focus()
+localStorage.setItem("ListadoServ", JSON.stringify(listadoServ))
 })
 
 btnCancelar.addEventListener("click", ()=>{
@@ -14,9 +15,13 @@ inputServicio.value = ""
 inputImporte.value = ""
 })
 
-function recuperLS (){
-servicios = JSON.parse(localStorage.getItem("servicios"))
-listadoServ = JSON.parse(localStorage.getItem("ListadoServ"))
-}
+// function recuperLS (){
+// servicios = JSON.parse(localStorage.getItem("servicios"))
+// listadoServ = JSON.parse(localStorage.getItem("ListadoServ"))
+// }
 
-recuperLS()
+// recuperLS()
+
+
+
+
